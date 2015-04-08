c58b4d1b6121c1adaac4704e67ded9b4643ebb57
AndyRuan <haodongruan@dhcp-10-132-180-229.uawifi.arizona.edu>
2015-04-07 17:07:38 -0700

    task2-2

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var app = angular
+  .module('TaskNinjaApp', [
+    'ngAnimate',
+    'ngResource',    
+    'ngRoute',    
+    'firebase'
+  ])
+  .constant('FURL', 'https://tutormvp.firebaseio.com/')  
+  .config(function ($routeProvider) {
+    $routeProvider      
+      .when('/', {
+        templateUrl: 'views/main.html'        
+      })
+      .when('/post',{
+        templateUrl: 'views/post.html',
+        controller: 'TaskController'
+      })
+      .when('/edit/:taskId',{
+        templateUrl: 'views/edit.html',
+        controller: 'TaskController'
+      })
+      .when('/browse',{
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'
+      })
+      .when('/register',{
+        templateUrl: 'views/register.html',
+
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
